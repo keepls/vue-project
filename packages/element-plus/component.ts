fbe7ef087397916eafc4b1c0e16402fc008c1b1a
江聪 <keepjsls@163.com>
2022-04-18 11:45:07 +0800

    packages

---
+++ packages/element-plus/component.ts
@@ -0,0 +1,10 @@
+// import { ElAvatar } from '@element-plus/components/avatar'
+import {ElAvatar} from '@element-plus/components/avatar'
+
+
+import type {Plugin} from 'vue'
+
+export default [
+    ElAvatar
+] as Plugin[]
+
--- src/components/avatar/avatar.vue
+++ src/components/avatar/avatar.vue
@@ -1,4 +1,56 @@
 <template>
+    <span :class="avatarClass" :style="sizeStyle">
+        <img v-if="(src || srcSet) && !hasLoadError" :src="src" :alt="alt" :srcset="srcSet" :style="fitStyle" @error="handleError">
+        <el-icon v-else-if="icon"> 
+            <component :is="icon" />
+        </el-icon>
+        <slot v-else />
+    </span>
 </template>
 
-<script setup></script>
+<script lang="ts" setup>
+import {computed,ref,watch} from 'vue'
+import {ElIcon} from '@element-plus/components/icon'
+import {useNamespace} from '@element-plus/hooks'
+import {addUnit,isNumber,isString} from "@element-plus/utils"
+import {aavatarEmits,avatarProps} from './avatar'
+import type {CSSProperties} from 'vue'
+defineOptions({
+    name:"ElAvatar"
+})
+const props=defineProps(avatarProps)
+const emit=defineEmits(aavatarEmits)
+const ns=useNamespace('avatar')
+const hasLoadError=ref(false)
+
+const avatarClass=computed(()=>{
+    const {size,icon,shape}=props
+    const classList=[ns.b()]
+    if(isString(size)) classList.push(ns.m(size))
+    if(icon) classList.push(ns.m('icon'))
+    if(shape) classList.push(ns.m(shape))
+    return classList
+})
+const sizeStyle=computed(()=>{
+    const {size} =props
+    return isNumber(size) ? ({'--el-avatar-size':addUnit(size)} as CSSProperties) : undefined
+})
+
+const fitStyle=computed<CSSProperties>(()=>({
+    objectFit:props.fit
+}))
+
+watch(
+    ()=>props.src,
+    ()=>(hasLoadError.value=false)
+)
+
+function handleError(e:Event){
+    hasLoadError.value=true
+    emit('error',e)
+}
+
+</script>
+
+
+
